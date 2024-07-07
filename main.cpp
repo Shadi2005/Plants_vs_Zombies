c96a040898df8e9888eccf64547b0b77329a04e8
shadikhansari1384@gmail.com <shadikhansari1384@gmail.com>
2024-07-07 11:05:10 +0330

    commit some unnoticable changes in order to merge the updates

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include "signup.h"
 #include "game.h"
 #include "braincontainer.h"
 #include "suncontainer.h"
@@ -12,5 +13,9 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     game = new Game();
     game->show();
+
+    // SignUp sign_up_page;
+    // sign_up_page.show();
+
     return a.exec();
 }
