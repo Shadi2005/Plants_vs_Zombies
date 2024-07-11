a79935e7ef450f781a7aa22a8d1b0e8708da7993
Hoora Akhlaghi <hakhlaghi83@gmail.com>
2024-07-11 15:06:41 +0330

    design first page of game

--- Client/setip.cpp
+++ Client/setip.cpp
@@ -3,6 +3,8 @@
 #include "socket.h"
 #include "signup.h"
 #include <QMessageBox>
+#include <QStyle>
+
 
 extern Socket * socket;
 
@@ -11,6 +13,26 @@ setIP::setIP(QWidget *parent)
     , ui(new Ui::setIP)
 {
     ui->setupUi(this);
+    setFixedSize(700,700);
+
+    ui->label->setPixmap(QPixmap(":/pages/images/pages/set ip Background.jpg"));
+    ui->label->setGeometry(0, 0, 700, 700);
+
+    ui->label->stackUnder(ui->lineEdit);
+    ui->label->stackUnder(ui->pushButton);
+
+    ui->lineEdit->setGeometry(175,400,350,50);
+    ui->lineEdit->setPlaceholderText("Enter server IP");
+    QPalette palette;
+    palette.setColor(QPalette::Base, QColor(154, 217, 77,150)); // Light green
+    ui->lineEdit->setPalette(palette);
+    ui->lineEdit->setFrame(false);
+
+
+    ui->pushButton->setGeometry(300,480,80,40);
+    ui->pushButton->setStyleSheet("color: white; background-color: rgb(27, 120, 45);");
+    ui->lineEdit->setPalette(palette);
+
 }
 
 setIP::~setIP()
